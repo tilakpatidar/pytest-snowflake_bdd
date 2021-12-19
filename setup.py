dacbf5aea01b03fc84cbf2f0f19116071e4bc147
tilakpatidar <tilakpatidar@gmail.com>
2021-12-19 19:07:48 +0530

    Release version 0.1.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 gh_run_number = os.environ.get("GITHUB_RUN_NUMBER", None)
 build_number = None if gh_run_number is None or gh_run_number == "" else gh_run_number
 
-version = '0.1.0'
+version = '0.1.1'
 
 setup(
     name='pytest-snowflake_bdd',
