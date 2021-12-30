2e942de568e03e1711650d58dab38afd5c6cbf3c
tilakpatidar <tilakpatidar@gmail.com>
2021-12-30 14:13:30 +0530

    Bump version to 0.1.2

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 gh_run_number = os.environ.get("BUILD_NUMBER", None)
 build_number = None if gh_run_number is None or gh_run_number == "" else gh_run_number
 
-version = '0.1.1'
+version = '0.1.2'
 
 setup(
     name='pytest-snowflake_bdd',
